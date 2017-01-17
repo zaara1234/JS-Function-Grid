2fc6c9f4c4220159a3d01354500aa855ada7dcea
zaara afida <zafida.student@hsfi.us>
2017-01-17 21:49:38 +0000

    i put a smiley face in that chart.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //Write your function calls below this line
+setColor(2,1,"black");//
+setColor(2,2,"black");
+setColor(3,1,"blue");
+setColor(3,2,"pink");
+
+setColor(2,6,"black");//
+setColor(2,7,"black");
+setColor(3,6,"blue");
+setColor(3,7,"pink");
+
+setColor(6,1,"black");//
+setColor(7,2,"black");
+setColor(8,3,"blue");
+setColor(8,4,"pink");
+
+setColor(8,5,"black");//
+setColor(7,6,"black");
+setColor(6,7,"blue");
+
+
+
+
+
+
+
+
